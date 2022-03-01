148189cc46129615d0309ae1ee0fd5a2155f4bb5
Fredrik Björkman <pfl.bjorkman@gmail.com>
2022-03-01 15:13:09 +0100

    created simple get-put RocksDB application

--- flink-simplified/flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/rocksdbcount/RocksDBCount.java
+++ flink-simplified/flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/rocksdbcount/RocksDBCount.java
@@ -16,32 +16,73 @@
  * limitations under the License.
  */
 
-package org.apache.flink.streaming.examples.rocksdbtest;
+package org.apache.flink.streaming.examples.rocksdbcount;
 
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.examples.wordcount.WordCount;
+import org.apache.flink.util.Collector;
+
+import java.io.IOException;
 
 public class RocksDBCount {
+
+  public static class RocksSum extends RichFlatMapFunction<Tuple2<String, Integer>, Tuple2<String, Integer>> {
+
+    ValueState<Tuple2<String, Integer>> stateSum;
+
+
+    @Override
+    public void flatMap(Tuple2<String, Integer> value, Collector<Tuple2<String, Integer>> out) throws Exception {
+      Tuple2<String, Integer> current = stateSum.value();
+      if (current == null){
+        stateSum.update(new Tuple2<>(value.f0, value.f1));
+      } else{
+        current.f1 += value.f1;
+        stateSum.update(current);
+      }
+
+      System.out.println(stateSum.value());
+    }
+
+    @Override
+    public void open(Configuration config) throws IOException {
+      ValueStateDescriptor<Tuple2<String, Integer>> descriptor =
+          new ValueStateDescriptor<>("sum",
+                  TypeInformation.of(new TypeHint<>() {}));
+      stateSum = getRuntimeContext().getState(descriptor);
+    }
+  }
+
   public static void main(String[] args) throws Exception {
 
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
     env.setStateBackend(new EmbeddedRocksDBStateBackend());
+    env.getCheckpointConfig().setCheckpointStorage("file:////home/fredrik/Documents/MasterThesis/flink-simplified/flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/rocksdbcount/out/fink-checkpoints");
 
     DataStream<String> text = env.readTextFile("/home/fredrik/Documents/ScalableML/lab2/snil_data/SNLI_Corpus/snli_1.0_train.csv");
 
     DataStream<Tuple2<String, Integer>> counts =
             text.flatMap(new WordCount.Tokenizer())
+                    .filter(t -> t.f0.equals("and") | t.f0.equals("or"))
                     .keyBy(t -> t.f0)
                     .window(TumblingProcessingTimeWindows.of(Time.milliseconds(500)))
-                    .sum(1);
-//    counts.writeAsText("flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/rocksdbtest/out");
+                    .sum(1)
+                    .keyBy(t -> t.f0)
+                    .flatMap(new RocksSum());
+
     env.execute("RocksDBCount");
-    
+
   }
 }
